0dc79b5ab82a437994c8c6298479986229f253ed
jechandi <joseechandi@sodicaonline.com>
2016-09-07 16:54:17 -0400

    Test_Postgresql

---
+++ test_postgresql/test_postgresql/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using test_postgresql.clases;
+
+namespace test_postgresql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Master_List ml = new Master_List();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ml.psql_select().CopyToDataTable();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataSet ds2 = ml.psql_select_DS("Test1");
+            dataGridView1.DataSource = ml.psql_select_DS("Test1").Tables[0];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //solo para evitar duplicados en pruebas
+            int nv = ml.psql_count("Test1");
+            //solo para evitar duplicados en pruebas
+
+            //(1,'newval1'),(2,'newval2')   //string val = "(1,'newval1'),(2,'newval2')";
+
+            //sin AUTO_INCREMENT
+            //string val = "(" + (nv + 1) + ",'newval" + (nv + 1).ToString() + "'),(" + (nv + 2) + ",'newval" + (nv + 2).ToString() + "')";
+
+            //con AUTO_INCREMENT
+            string val = "('newval" + (nv + 1).ToString() + "'),('newval" + (nv + 2).ToString() + "')";
+            ml.psql_insert("Test1 (texto) ", val);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ test_postgresql/test_postgresql/clases/Master_List.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace test_postgresql.clases
+{
+    class Master_List
+    {
+        public static string conexion = string.Format("Server=localhost;Port=5432;User Id=Joec;Password=joec2107;Database=Test_Psql1;");
+        public NpgsqlConnection conn2 = new NpgsqlConnection(conexion);
+        public NpgsqlDataAdapter psqlad;
+
+        //-SELECT---------------------------------------------------------------------------
+
+        public List<System.Data.DataRow> psql_select()
+        {
+            DataSet ds = new DataSet();
+            string tabl = "Test1";
+            string query = "SELECT * FROM " + tabl + "";
+            psqlad = new NpgsqlDataAdapter(query, conn2);
+            psqlad.Fill(ds);
+            return ds.Tables[0].AsEnumerable().ToList();
+        }
+        
+        public DataSet psql_select_DS(string tabl)
+        {
+            DataSet ds = new DataSet();
+            string query = "SELECT * FROM " + tabl;
+            psqlad = new NpgsqlDataAdapter(query, conn2);
+            psqlad.Fill(ds);
+            return ds;
+        }
+
+        //----------------------------------------------------------------------------
+
+
+        //-INSERT---------------------------------------------------------------------------
+
+        //string tabl = "Test1";
+        //val = "(1,'newval1'),(2,'newval2')"
+        public bool psql_insert(string tabl, string val)
+        {
+            string query = "INSERT INTO " + tabl + " VALUES " + val;
+            conn2.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand(query, conn2);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            conn2.Close();
+            return true;
+        }
+
+
+
+
+
+
+        //string tabl = "Test1";
+        //string query = "INSERT INTO \"" + tabl + "\" VALUES (1,'newval1'),(2,'newval2')";
+
+        public void psql_query(string query)
+        {
+            conn2.Open();
+
+            NpgsqlCommand cmd = new NpgsqlCommand(query, conn2);
+
+            cmd.ExecuteNonQuery(); cmd.Dispose(); conn2.Close();
+        }
+
+        //----------------------------------------------------------------------------
+
+        
+
+        //----------------------------------------------------------------------------
+
+        public void psql_insert_parametros()
+        {
+            string tabl = "Test1";
+            string query = "INSERT INTO " + tabl + " VALUES (@id,@texto)";
+
+            conn2.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = conn2; cmd.CommandText = query; cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.Add(new NpgsqlParameter("@id", 7));
+            cmd.Parameters.Add(new NpgsqlParameter("@texto", "newval7"));
+
+            cmd.ExecuteNonQuery(); cmd.Dispose(); conn2.Close();
+        }
+
+        //----------------------------------------------------------------------------
+
+        
+
+        public int psql_count(string tabl)
+        {
+            int a = 0;
+            conn2.Open();
+            //NpgsqlCommand cmd = new NpgsqlCommand(query, conn2);
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT COUNT(*) FROM "+ tabl, conn2);
+            NpgsqlDataReader read1;
+            read1 = cmd.ExecuteReader();
+            while (read1.Read()){
+                a = read1.GetInt16(0);
+            }
+            read1.Close(); cmd.Dispose(); conn2.Close();
+            return a;
+        }
+    }
+}
